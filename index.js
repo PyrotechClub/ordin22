94dfebf04816350d0e74c8998e93b0058081a722
sakina2005nadeem <sakinadeem2.0@gmail.com>
2022-06-24 18:45:13 +0530

    homepage, events, update page

--- index.js
+++ index.js
@@ -44,3 +44,32 @@ function readMore(comp) {
     moreText2.style.display = 'inline';
   }
 }
+
+//TIMER
+
+const countdown = () => {
+  const countDate = new Date('August 3, 2022 00:00:00').getTime();
+  const now = new Date().getTime();
+  const gap = countDate - now;
+
+  //TIME WORK
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  //CALCULATION
+  const textDay = Math.floor(gap / day);
+  const textHour = Math.floor((gap % day) / hour);
+  const textMinute = Math.floor((gap % hour) / minute);
+  const textSecond = Math.floor((gap % minute) / second);
+
+  console.log(textDay, textHour, textMinute, textSecond);
+
+  document.querySelector('.day').innerText = textDay;
+  document.querySelector('.hour').innerText = textHour;
+  document.querySelector('.minute').innerText = textMinute;
+  document.querySelector('.second').innerText = textSecond;
+};
+
+setInterval(countdown, 1000);
