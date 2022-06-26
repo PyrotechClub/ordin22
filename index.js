846b2e5388250a7434d6f62e45db57d2dd193532
sakina2005nadeem <sakinadeem2.0@gmail.com>
2022-06-26 18:38:56 +0530

    changed colors thode aur

--- index.js
+++ index.js
@@ -64,8 +64,6 @@ const countdown = () => {
   const textMinute = Math.floor((gap % hour) / minute);
   const textSecond = Math.floor((gap % minute) / second);
 
-  console.log(textDay, textHour, textMinute, textSecond);
-
   document.querySelector('.day').innerText = textDay;
   document.querySelector('.hour').innerText = textHour;
   document.querySelector('.minute').innerText = textMinute;
@@ -73,3 +71,5 @@ const countdown = () => {
 };
 
 setInterval(countdown, 1000);
+
+//VANTA JS
