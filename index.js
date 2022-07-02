872161c2e0b1dfdb7edb800542e3306e5754e946
Naman Chandok <namanchandok1@gmail.com>
2022-07-02 12:14:04 +0530

    .

--- index.js
+++ index.js
@@ -3,12 +3,11 @@
 //LOADER
 var myVar;
 function load() {
-  myVar = setTimeout(showPage, 1500);
+  myVar = setTimeout(showPage, 3000);
 }
 
 function showPage() {
-  document.getElementById('load').style.display = 'none';
-  document.getElementById('mainBod').style.display = 'block';
+  document.getElementById("load").style.transform = "translateY(-500vh)";
 }
 
 //NAVBAR
@@ -23,30 +22,3 @@ jQuery(document).on('scroll', function () {
     $('.nav-logo').css('filter', 'invert(100%)');
   }
 });
-
-//TIMER
-
-const countdown = () => {
-  const countDate = new Date('August 3, 2022 00:00:00').getTime();
-  const now = new Date().getTime();
-  const gap = countDate - now;
-
-  //TIME WORK
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-
-  //CALCULATION
-  const textDay = Math.floor(gap / day);
-  const textHour = Math.floor((gap % day) / hour);
-  const textMinute = Math.floor((gap % hour) / minute);
-  const textSecond = Math.floor((gap % minute) / second);
-
-  document.querySelector('.day').innerText = textDay;
-  document.querySelector('.hour').innerText = textHour;
-  document.querySelector('.minute').innerText = textMinute;
-  document.querySelector('.second').innerText = textSecond;
-};
-
-setInterval(countdown, 1000);
